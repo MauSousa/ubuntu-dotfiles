eabee405609fc3e0cfb5a09184e441e38b1e14f3
Augusto Sousa <lagrange.tesla@gmail.com>
2021-08-10 12:57:47 -0500

    Added symbol for wifi

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -14,6 +14,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar log -c ~/.config/polybar/current.ini &
 polybar secondary -c ~/.config/polybar/current.ini &
 polybar terciary -c ~/.config/polybar/current.ini &
+polybar quaternary -c ~/.config/polybar/current.ini &
 
 ## Right bar
 polybar top -c ~/.config/polybar/current.ini &
