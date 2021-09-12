961912c850a379eaa621ad7b0ce3eebf8ba395fe
Augusto Sousa <lagrange.tesla@gmail.com>
2021-09-11 20:26:34 -0500

    Deleted script

--- config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-echo '###########################'
-echo "   You should be root user "
-echo "   Installing Dotfiles!    "
-echo "   Upgrading the system    "
-echo '###########################'
-sudo apt update && sudo apt upgrade
